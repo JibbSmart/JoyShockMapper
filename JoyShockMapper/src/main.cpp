f81e617628ee9397e9070e1719f84f592c3ffbea
JibbSmart <smartjibb@hotmail.com>
2019-03-18 12:33:47 +0800

    No longer start with continuous calibration enabled by default. Changed version number in anticipation of next changes before publishing a release

--- JoyShockMapper/src/main.cpp
+++ JoyShockMapper/src/main.cpp
@@ -16,7 +16,7 @@
 // C increases when all that's happened is some bugs have been fixed.
 // B increases and C resets to 0 when new features have been added.
 // A increases and B and C reset to 0 when major new features have been added that warrant a new major version, or replacing older features with better ones that require the user to interact with them differently
-const char* version = "1.0.2";
+const char* version = "1.1.0";
 
 #define PI 3.14159265359f
 
@@ -166,7 +166,7 @@ public:
 	float delta_flick = 0.0;
 	float flick_percent_done = 0.0;
 	float flick_rotation_counter = 0.0;
-	bool toggleContinuous = true;
+	bool toggleContinuous = false;
 
 	float poll_rate;
 	int controller_type = 0;
@@ -1515,7 +1515,7 @@ void connectDevices() {
 		handle_to_joyshock.emplace(deviceHandles[i], js);
 
 		// calibration?
-		JslStartContinuousCalibration(deviceHandles[i]);
+		//JslStartContinuousCalibration(deviceHandles[i]);
 	}
 
 	if (numConnected == 1) {
@@ -1525,9 +1525,9 @@ void connectDevices() {
 		printf("%i devices connected\n", numConnected);
 	}
 
-	if (numConnected != 0) {
-		printf("All devices have started continuous gyro calibration\n");
-	}
+	//if (numConnected != 0) {
+	//	printf("All devices have started continuous gyro calibration\n");
+	//}
 
 	delete[] deviceHandles;
 }
