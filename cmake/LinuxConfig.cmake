1ed75a83261870a869011fc263b59ebbd506feef
Romeo Calota <kicsyromy@gmail.com>
2020-07-12 08:28:42 +0300

    Add install targets

--- cmake/LinuxConfig.cmake
+++ cmake/LinuxConfig.cmake
@@ -22,4 +22,60 @@ if (UNIX AND NOT APPLE)
     )
 
 	add_library (Platform::Dependencies ALIAS platform_dependencies)
+	
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/50-joyshockmapper.rules
+        DESTINATION lib/udev/rules.d
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/JoyShockMapper/gyro_icon.png
+        DESTINATION share/icons/hicolor/512x512/apps
+        RENAME JoyShockMapper.png
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/JoyShockMapper.desktop
+        DESTINATION share/applications
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/jsm-status.svg
+        DESTINATION share/icons/hicolor/16x16/status
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/jsm-status-dark.svg
+        DESTINATION share/icons/hicolor/16x16/status
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/jsm-status.svg
+        DESTINATION share/icons/hicolor/22x22/status
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/jsm-status-dark.svg
+        DESTINATION share/icons/hicolor/22x22/status
+    )
+    
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/jsm-status.svg
+        DESTINATION share/icons/hicolor/24x24/status
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/jsm-status-dark.svg
+        DESTINATION share/icons/hicolor/24x24/status
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/jsm-status.svg
+        DESTINATION share/icons/hicolor/scalable/status
+    )
+
+    install (
+        FILES ${PROJECT_SOURCE_DIR}/dist/linux/jsm-status-dark.svg
+        DESTINATION share/icons/hicolor/scalable/status
+    )
 endif ()
