28cc1ec1fa3033f2002fa30e22daa44023984daf
rstraver <roystraver@gmail.com>
2020-08-30 17:25:54 +0200

    First test version for FLICK_SPEED

--- JoyShockMapper/src/main.cpp
+++ JoyShockMapper/src/main.cpp
@@ -45,6 +45,7 @@ JSMSetting<AxisMode> aim_y_sign = JSMSetting<AxisMode>(SettingID::STICK_AXIS_Y, 
 JSMSetting<AxisMode> gyro_y_sign = JSMSetting<AxisMode>(SettingID::GYRO_AXIS_Y, AxisMode::STANDARD);
 JSMSetting<AxisMode> gyro_x_sign = JSMSetting<AxisMode>(SettingID::GYRO_AXIS_X, AxisMode::STANDARD);
 JSMSetting<float> flick_time = JSMSetting<float>(SettingID::FLICK_TIME, 0.1f);
+JSMSetting<float> flick_speed = JSMSetting<float>(SettingID::FLICK_SPEED, 0.0f);
 JSMSetting<float> gyro_smooth_time = JSMSetting<float>(SettingID::GYRO_SMOOTH_TIME, 0.125f);
 JSMSetting<float> gyro_smooth_threshold = JSMSetting<float>(SettingID::GYRO_SMOOTH_THRESHOLD, 0.0f);
 JSMSetting<float> gyro_cutoff_speed = JSMSetting<float>(SettingID::GYRO_CUTOFF_SPEED, 0.0f);
@@ -686,6 +687,9 @@ public:
 			case SettingID::FLICK_TIME:
 				opt = flick_time.get(*activeChord);
 				break;
+			case SettingID::FLICK_SPEED:
+				opt = flick_speed.get(*activeChord);
+				break;
 			case SettingID::GYRO_SMOOTH_THRESHOLD:
 				opt = gyro_smooth_threshold.get(*activeChord);
 				break;
@@ -1294,6 +1298,7 @@ static void resetAllMappings() {
 	gyro_y_sign.Reset();
 	gyro_x_sign.Reset();
 	flick_time.Reset();
+	flick_speed.Reset();
 	gyro_smooth_time.Reset();
 	gyro_smooth_threshold.Reset();
 	gyro_cutoff_speed.Reset();
@@ -1600,6 +1605,13 @@ static float handleFlickStick(float calX, float calY, float lastCalX, float last
 	// do the flicking
 	float secondsSinceFlick = ((float)chrono::duration_cast<chrono::microseconds>(jc->time_now - jc->started_flick).count()) / 1000000.0f;
 	float newPercent = secondsSinceFlick / jc->getSetting(SettingID::FLICK_TIME);
+
+	float delta_weight = abs(jc->delta_flick) * jc->getSetting(SettingID::FLICK_SPEED);
+	if (delta_weight > 0.0f) {
+		newPercent = newPercent / delta_weight;
+		// printf("Flick: %.3f delta_flick\n", abs(jc->delta_flick));
+	}
+
 	if (newPercent > 1.0f) newPercent = 1.0f;
 	// warping towards 1.0
 	float oldShapedPercent = 1.0f - jc->flick_percent_done;
@@ -2482,6 +2494,8 @@ int main(int argc, char *argv[]) {
 	commandRegistry.Add((new GyroSensAssignment("GYRO_SENS", max_gyro_sens))->SetHelp(""));
 	commandRegistry.Add((new JSMAssignment<float>(flick_time))
 		->SetHelp("Sets how long a flick takes in seconds. This value is used by stick FLICK mode."));
+	commandRegistry.Add((new JSMAssignment<float>(flick_speed))
+		->SetHelp("Divides how long a flick takes by its radian delta. This value is used by stick FLICK mode."));
 	commandRegistry.Add((new JSMAssignment<float>(gyro_smooth_threshold))
 		->SetHelp("When the controller's angular velocity is below this threshold (in degrees per second), smoothing will be applied."));
 	commandRegistry.Add((new JSMAssignment<float>(gyro_smooth_time))
